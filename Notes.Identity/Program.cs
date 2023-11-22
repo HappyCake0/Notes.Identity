1572ff4f93ae11438c5a1e8ebbed9189747af56c
Илья Чепурин <chepurin.ilya908@mail.ru>
2023-11-22 15:52:25 +0300

    Добавьте файлы проекта.

---
+++ Notes.Identity/Program.cs
@@ -0,0 +1,73 @@
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using Notes.Identity;
+using Notes.Identity.Data;
+using Notes.Identity.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetValue<string>("DbConnection");
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+{
+    options.UseSqlite(connectionString);
+});
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(config =>
+{
+    config.Password.RequiredLength = 4;
+    config.Password.RequireDigit = false;
+    config.Password.RequireDigit = false;
+    config.Password.RequireUppercase = false;
+})
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<AppUser>()
+    .AddInMemoryApiResources(Configuration.ApiResources)
+    .AddInMemoryIdentityResources(Configuration.IdentityResources)
+    .AddInMemoryApiScopes(Configuration.ApiScopes)
+    .AddInMemoryClients(Configuration.Clients)
+    .AddDeveloperSigningCredential();
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.ConfigureApplicationCookie(config =>
+{
+    config.Cookie.Name = "Notes.Identity.Cookie";
+    config.LoginPath = "/Auth/Login";
+    config.LogoutPath = "/Auth/Logout";
+});
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<AuthDbContext>();
+        DbInittializer.Initialize(context);
+    }
+    catch (Exception exception)
+    {
+        var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, "An error occured while app initialization");
+    }
+}
+
+
+app.UseIdentityServer();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(app.Environment.ContentRootPath, "Styles")), RequestPath = "/styles"
+});
+
+app.MapDefaultControllerRoute();
+
+
+
+app.Run();
---
+++ Notes.Identity/Styles/EyeScript.js
@@ -0,0 +1,11 @@
+﻿const passField = document.getElementById("pasField");
+const showBtn = document.getElementById("eye");
+showBtn.onclick = (() => {
+    if (passField.type === "password") {
+        passField.type = "text";
+        showBtn.classList.add("hide-btn");
+    } else {
+        passField.type = "password";
+        showBtn.classList.remove("hide-btn");
+    }
+});
